7c799cf5e511f4af17a38bcca0e0e8afefd5a475
VCassina <victorcassina@gmail.com>
2023-07-05 22:16:14 +0200

    Few changes to manage it.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from 'react-router-dom';
 import "../src/styles/css/main.css";
 
-const root = ReactDOM.createRoot(document.getElementById("reactRoot"));
+const root = createRoot(document.getElementById("reactRoot"));
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
