33e1b49a40e23adfc321b51d26eb61cbaf53dbaa
VCassina <victorcassina@gmail.com>
2023-07-12 16:24:04 +0200

    Last fix before tablette responsive - Font update / content update / temp fixe added to myCreations.

--- src/components/CreationsBottomArticle.js
+++ src/components/CreationsBottomArticle.js
@@ -1,20 +1,19 @@
 import React from "react";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { Markup } from 'interweave';
+import { Markup } from "interweave";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faGlobe } from "@fortawesome/free-solid-svg-icons";
-import TEMP_FIX_IMG from "../assets/NinaCarducci.webp"
 library.add(faGlobe);
 
-function CreationsBottomArticle ({
+function CreationsBottomArticle({
   webSiteScreen,
   altScreen,
   description,
   animateOut,
   title,
   siteUrl,
-  gitUrl
+  gitUrl,
 }) {
   const classNames = `creations_content_carrousel ${
     animateOut ? "slideout-animation" : "slidein-animation"
@@ -24,22 +23,31 @@ function CreationsBottomArticle ({
     <div className={classNames}>
       <>
         <img
-        // src={webSiteScreen}
-          src={TEMP_FIX_IMG}
+          src={webSiteScreen}
           alt={altScreen}
           className="creations_content_carrousel-img"
         />
         <div className="creations_content_carrousel-txt">
           <div className="creations_content_carrousel-txt-title">
-            <h3 className="creations_content_carrousel-txt-title-main">{title}</h3>
+            <h3 className="creations_content_carrousel-txt-title-main">
+              {title}
+            </h3>
             <span className="creations_content_carrousel-txt-title-subtitle"></span>
           </div>
           <div className="creations_content_carrousel-txt-links">
-            <a href={gitUrl}><FontAwesomeIcon icon={faGithub} /></a>
-            <a href={siteUrl}><FontAwesomeIcon icon={faGlobe} /></a>
+          {gitUrl && (
+            <a href={gitUrl}>
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          )}
+          {siteUrl && (
+            <a href={siteUrl}>
+              <FontAwesomeIcon icon={faGlobe} />
+            </a>
+          )}
           </div>
           <div className="creations_content_carrousel-txt-description">
-          <Markup content={description} />
+            <Markup content={description} />
           </div>
         </div>
       </>
--- src/components/MyCreations.js
+++ src/components/MyCreations.js
@@ -4,35 +4,42 @@ import AnchorTarget from "../items/AnchorTarget";
 import CreationsUpperArticle from "../components/CreationsUpperArticle";
 import CreationsBottomArticle from "../components/CreationsBottomArticle";
 import titleAnimationHelper from "../helpers/titleAnimationHelper";
+import bankArgent from "../assets/BankArgent.webp";
+import ninaCarducci from "../assets/NinaCarducci.webp";
+import devBootes from "../assets/BootesDev.webp";
 
 function MyCreations() {
+  const imgSrc = [bankArgent, ninaCarducci, devBootes];
   const dataSlider = data;
   const sleepingRef = useRef(null);
   const [selectedElement, setSelectedElement] = useState(0);
   const [animateOut, setAnimateOut] = useState(false);
   const [nextElement, setNextElement] = useState(null);
   const [newData, setNewData] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(0);
 
   titleAnimationHelper(
     "creations_content_upper-title-sleepingAnimation",
     sleepingRef
   );
-  // Gestion des animations du carrousel.
+
   const handleElementChange = (index) => {
     if (selectedElement === index) {
       return null;
-    } else {
     }
+
     setAnimateOut(true);
     setSelectedElement(index);
+
     setTimeout(() => {
+      setSelectedImage(index);
       setAnimateOut(false);
     }, 350);
   };
 
-  // Gestion de la mise à jour des données du carrousel & gestion du temps.
   useEffect(() => {
     let timeoutId = null;
+
     if (selectedElement !== null) {
       if (timeoutId) {
         clearTimeout(timeoutId);
@@ -41,14 +48,14 @@ function MyCreations() {
         setNewData(dataSlider[selectedElement]);
       }, 350);
     }
+
     return () => clearTimeout(timeoutId);
   }, [selectedElement, dataSlider]);
 
   return (
     <article className="creations_container">
-       <AnchorTarget id="creations" />
+      <AnchorTarget id="creations" />
       <div className="importantComponent">
-       
         <div className="creations_content">
           <div className="creations_content_upper">
             <h2 className="creations_content_upper-title">
@@ -72,7 +79,7 @@ function MyCreations() {
 
           <div className="creations_content_carrousel">
             <CreationsBottomArticle
-              webSiteScreen={newData?.webSiteScreen}
+              webSiteScreen={imgSrc[selectedImage]}
               altScreen={newData?.altScreen}
               description={newData?.description}
               title={newData?.title}
