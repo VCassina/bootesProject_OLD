fdf1c530ed7fdf2de49054ca3c4e7b124d704eee
VCassina <victorcassina@gmail.com>
2023-07-14 13:05:04 +0200

    STOP to deploy the React Query global state manager.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Routes from "./Routes";
 import Header from "./components/Header";
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
     <Header />
     <Routes />
-    </>
+    </QueryClientProvider>
   );
 }
 
---
+++ src/store.js
@@ -0,0 +1,9 @@
+let tabletteDisplay = false;
+
+export function isTabletteDisplay() {
+  return tabletteDisplay;
+}
+
+export function setTabletteDisplay(newValue) {
+  tabletteDisplay = newValue;
+}
