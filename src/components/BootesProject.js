a8c65f46d18b238fdfe1aee7576c300f8d1010e8
VCassina <victorcassina@gmail.com>
2023-07-16 23:00:15 +0200

    Starting the IMG optimization.

--- src/components/BootesProject.js
+++ src/components/BootesProject.js
@@ -24,7 +24,6 @@ function BootesProject() {
               src={Logo}
               alt="Bannière avec le logo de Boötes Dev"
               className="bootes_icn-img"
-              style={{ width: "250px", height: "auto" }}
             />
           </div>
         ) : null}
@@ -67,7 +66,8 @@ function BootesProject() {
               ${lowDesktopMeByCatalysStuff} 1366w,
               ${meByCatalysStuff} 1920w`}
               sizes="(max-width: 767px) 200px, (max-width: 1366) 300px, (min-width: 1367) 380px"
-              style={{ minWidth: "100%", height: "auto" }}
+              width="380"
+              height="435"
             />
             <figcaption>Designed by CatalysStuff - Thank you !</figcaption>
           </figure>
