4f2aa39fc6f0de325e8338554f73695e83ee4ed1
VCassina <victorcassina@gmail.com>
2023-07-17 02:41:05 +0200

    Rewind SASS after seoOptimization.

--- src/items/BootesArticle.js
+++ src/items/BootesArticle.js
@@ -18,17 +18,17 @@ function BootesArticle({
       {tabletteDisplay && data ? (
         <div className="bootes_titleBundle">
           {data.map((item, index) => (
-            <h3
+            <h2
               key={index}
               onClick={() => setSelectedItemIndex(index)}
               className={index === selectedItemIndex ? "active bootes_content-article-title" : "unactive bootes_content-article-title"}
             >
               {item.titleShort}
-            </h3>
+            </h2>
           ))}
         </div>
       ) : (
-        <h3 className={`bootes_content-article-title ${titleIndex}`}>{title}</h3>
+        <h2 className={`bootes_content-article-title ${titleIndex}`}>{title}</h2>
       )}
       <div className="bootes_content-article-txt">
         <Markup content={description} />
